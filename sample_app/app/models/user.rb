2ec59e379b8eda9596242bdaa317059213e9bd9b
otinu <fitshter@gmail.com>
2021-04-25 14:45:55 +0000

    [Finish] Chapter6

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
+    before_save { self.email = email.downcase }
     validates :name, presence: true, length: { maximum: 50}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :email, 
                presence: true, 
                length: { maximum: 255 }, 
                format: { with: VALID_EMAIL_REGEX },
-               uniqueness: { case_sensitive: false }
+               uniqueness: true   #case_sensitive で強制的にfalseにさせていたが、必要がなくなったためにtrueに戻した。
+    has_secure_password
+    validates :password, presence: true, length: { minimum: 6 }
 end
