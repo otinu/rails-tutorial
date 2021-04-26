896b9d02aa921a91d2ed153ca73d12b546ad9ad5
otinu <fitshter@gmail.com>
2021-04-26 13:02:54 +0000

    [Finish]Chapter8

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -9,4 +9,10 @@ class User < ApplicationRecord
                uniqueness: true   #case_sensitive で強制的にfalseにさせていたが、必要がなくなったためにtrueに戻した。
     has_secure_password
     validates :password, presence: true, length: { minimum: 6 }
+    
+    def User.digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
 end
